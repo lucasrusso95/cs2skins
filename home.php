8df9abc5b42b36e99fd68bc4a17342438a5c412a
Lucas Russo <lucasrusso95@gmail.com>
2024-10-04 10:40:13 -0300

    change site pt7

--- home.php
+++ home.php
@@ -13,22 +13,22 @@
 </head>
     <body>
         <div>
-            <div style="display: flex;justify-content: center;margin: 40px 0;">
+            <div style="display: flex;justify-content: center;margin: 20px 0 40px 0;">
                 <img src="images/bg-quest.jpg" width="500" alt="quest lan house" style="border-radius: 25px;" />
             </div>
             <div style="display: flex; gap: 20px; flex-wrap: wrap; margin: 0 20px;">
-                <img src="images/foto-1.jpg" width="200" height="200" style="border-radius: 100%;" />
-                <img src="images/foto-2.jpg" width="200" height="200" style="border-radius: 100%;" />
-                <img src="images/foto-3.jpg" width="200" height="200" style="border-radius: 100%;" />
-                <img src="images/foto-4.jpg" width="200" height="200" style="border-radius: 100%;" />
-                <img src="images/foto-5.jpg" width="200" height="200" style="border-radius: 100%;" />
-                <img src="images/foto-6.jpg" width="200" height="200" style="border-radius: 100%;" />
-                <img src="images/foto-7.jpg" width="200" height="200" style="border-radius: 100%;" />
-                <img src="images/foto-8.jpg" width="200" height="200" style="border-radius: 100%;" />
-                <img src="images/foto-9.jpg" width="200" height="200" style="border-radius: 100%;" />
-                <img src="images/foto-10.jpg" width="200" height="200" style="border-radius: 100%;" />
+                <img src="images/foto-1.jpg" width="150" height="150" style="border-radius: 100%;" />
+                <img src="images/foto-2.jpg" width="150" height="150" style="border-radius: 100%;" />
+                <img src="images/foto-3.jpg" width="150" height="150" style="border-radius: 100%;" />
+                <img src="images/foto-4.jpg" width="150" height="150" style="border-radius: 100%;" />
+                <img src="images/foto-5.jpg" width="150" height="150" style="border-radius: 100%;" />
+                <img src="images/foto-6.jpg" width="150" height="150" style="border-radius: 100%;" />
+                <img src="images/foto-7.jpg" width="150" height="150" style="border-radius: 100%;" />
+                <img src="images/foto-8.jpg" width="150" height="150" style="border-radius: 100%;" />
+                <img src="images/foto-9.jpg" width="150" height="150" style="border-radius: 100%;" />
+                <img src="images/foto-10.jpg" width="150" height="150" style="border-radius: 100%;" />
             </div>
-            <div class="bg-primary" style="display: flex;gap: 20px;justify-content: center;align-items: center;margin-top: 1rem;">
+            <div class="bg-primary" style="display: flex;gap: 20px;justify-content: center;align-items: center;margin-top: 40px;">
                 <h2>To choose weapon paints loadout, you need to</h2>
                 <?php loginbutton(); ?>
             </div>
