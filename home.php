67a524a42ed9c55a24b528880816186113e365f7
Lucas Russo <lucasrusso95@gmail.com>
2024-10-04 10:35:05 -0300

    change site pt4

--- home.php
+++ home.php
@@ -17,16 +17,16 @@
                 <img src="images/bg-quest.jpg" width="500" alt="quest lan house"/>
             </div>
             <div style="display: flex; gap: 20px; flex-wrap: wrap; margin: 0 20px;">
-                <img src="images/foto-1.jpg" height="200" />
-                <img src="images/foto-2.jpg" height="200" />
-                <img src="images/foto-3.jpg" height="200" />
-                <img src="images/foto-4.jpg" height="200" />
-                <img src="images/foto-5.jpg" height="200" />
-                <img src="images/foto-6.jpg" height="200" />
-                <img src="images/foto-7.jpg" height="200" />
-                <img src="images/foto-8.jpg" height="200" />
-                <img src="images/foto-9.jpg" height="200" />
-                <img src="images/foto-10.jpg" height="200" />
+                <img src="images/foto-1.jpg" height="200" style="border-radius: 100%;" />
+                <img src="images/foto-2.jpg" height="200" style="border-radius: 100%;" />
+                <img src="images/foto-3.jpg" height="200" style="border-radius: 100%;" />
+                <img src="images/foto-4.jpg" height="200" style="border-radius: 100%;" />
+                <img src="images/foto-5.jpg" height="200" style="border-radius: 100%;" />
+                <img src="images/foto-6.jpg" height="200" style="border-radius: 100%;" />
+                <img src="images/foto-7.jpg" height="200" style="border-radius: 100%;" />
+                <img src="images/foto-8.jpg" height="200" style="border-radius: 100%;" />
+                <img src="images/foto-9.jpg" height="200" style="border-radius: 100%;" />
+                <img src="images/foto-10.jpg" height="200" style="border-radius: 100%;" />
             </div>
             <div class="bg-primary" style="display: flex;gap: 20px;justify-content: center;align-items: center;margin-top: 1rem;">
                 <h2>To choose weapon paints loadout, you need to</h2>
