f036acaefeb77531c05969b470a3c6b120fe71c1
Lucas Russo <lucasrusso95@gmail.com>
2024-10-04 10:33:29 -0300

    change site pt3

--- home.php
+++ home.php
@@ -16,19 +16,19 @@
             <div style="display: flex;justify-content: center;margin: 40px 0;">
                 <img src="images/bg-quest.jpg" width="500" alt="quest lan house"/>
             </div>
-            <div style="display: flex; gap: 10px; margin: 0 20px;">
-                <img src="images/foto-1.jpg" width="210" />
-                <img src="images/foto-2.jpg" width="210" />
-                <img src="images/foto-3.jpg" width="210" />
-                <img src="images/foto-4.jpg" width="210" />
-                <img src="images/foto-5.jpg" width="210" />
-                <img src="images/foto-6.jpg" width="210" />
-                <img src="images/foto-7.jpg" width="210" />
-                <img src="images/foto-8.jpg" width="210" />
-                <img src="images/foto-9.jpg" width="210" />
-                <img src="images/foto-10.jpg" width="210" />
+            <div style="display: flex; gap: 20px; flex-wrap: wrap; margin: 0 20px;">
+                <img src="images/foto-1.jpg" height="200" />
+                <img src="images/foto-2.jpg" height="200" />
+                <img src="images/foto-3.jpg" height="200" />
+                <img src="images/foto-4.jpg" height="200" />
+                <img src="images/foto-5.jpg" height="200" />
+                <img src="images/foto-6.jpg" height="200" />
+                <img src="images/foto-7.jpg" height="200" />
+                <img src="images/foto-8.jpg" height="200" />
+                <img src="images/foto-9.jpg" height="200" />
+                <img src="images/foto-10.jpg" height="200" />
             </div>
-            <div class="bg-primary">
+            <div class="bg-primary" style="display: flex;gap: 20px;justify-content: center;align-items: center;margin-top: 1rem;">
                 <h2>To choose weapon paints loadout, you need to</h2>
                 <?php loginbutton(); ?>
             </div>
